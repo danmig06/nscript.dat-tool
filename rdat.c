c94f9d16475e600220f6db8e3a1bcaf6fff07290
danmig06 <120124725+danmig06@users.noreply.github.com>
2023-02-06 15:50:33 +0100

    Add files via upload

---
+++ rdat.c
@@ -0,0 +1,55 @@
+#ifdef _WIN32
+#include <stdlib.h>
+#endif
+#ifdef __unix__
+#include <stdlib.h>
+#endif
+#include <stdio.h>
+#define bsize 32 // buffer size
+
+void write(FILE* src, FILE* out) {
+	int i;
+	char buffer[bsize];
+	while(1) {
+		int wq = fread(buffer, 1, bsize, src);
+		for(i = 0; i < bsize; i++)
+			buffer[i] ^= 0x84;
+		if(fwrite(buffer, wq, 1, out) == 0)
+			printf("failed to write\n");
+		if(feof(src))
+			break;
+	}
+	fclose(out);
+	fclose(src);
+	return;
+}
+
+void main() {
+	int i;
+	FILE* src = fopen("nscript.dat", "r+b");
+	if(src == NULL) {
+		printf("unable to find/open nscript.dat, searching for nscript.txt...\n");
+		src = fopen("nscript.txt", "r+b");
+		if(src == NULL) {
+			printf("not found. execution terminated\n");
+			return;
+		}
+		FILE* out = fopen("nscript.dat", "w+b");
+		if(out == NULL) {
+			printf("unable to create output file\n");
+			return;
+		}
+		write(src, out);
+		printf("nscript.txt encrypted in nscript.dat\n");
+		return;
+	}
+	FILE* out = fopen("nscript.txt", "w+b");
+	write(src, out);
+	printf("nscript.dat decrypted in nscript.txt\n");
+	#ifdef _WIN32
+	system("pause");
+	#endif
+	#ifdef __unix__
+	system("bash -c 'read -r -p \"Press any key to continue...\" && command'");
+	#endif
+}
