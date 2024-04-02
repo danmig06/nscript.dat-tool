b270f8fe47ef978787d799f2a96047eea2d53663
danmig06 <120124725+danmig06@users.noreply.github.com>
2024-04-02 14:40:18 +0200

    Update rdat.c
    it's been some time and the code was in an awful state

--- rdat.c
+++ rdat.c
@@ -1,21 +1,17 @@
-#ifdef _WIN32
-#include <stdlib.h> //pause at the end of the program
-#endif
-#ifdef __unix__
-#include <stdlib.h> //pause at the end of the program
-#endif
 #include <stdio.h>
-#define bsize 32 // buffer size
+#define bsize 32 // read/write buffer size
 
 void write(FILE* src, FILE* out) {
 	int i;
 	char buffer[bsize];
 	while(1) {
 		int wq = fread(buffer, 1, bsize, src);
-		for(i = 0; i < bsize; i++)
+		for(i = 0; i < (wq >= bsize) ? bsize : wq; i++)
 			buffer[i] ^= 0x84; //the algorithm inverts the 8th and 3rd bit (from right to left) in every single byte
-		if(fwrite(buffer, wq, 1, out) == 0)
-			printf("failed to write to file\n");
+		if(fwrite(buffer, wq, 1, out) == 0) {
+			puts("failed to write to file");
+			exit(1);
+		}
 		if(feof(src))
 			break;
 	}
@@ -24,37 +20,29 @@ void write(FILE* src, FILE* out) {
 	return;
 }
 
-void main() {
+int main(void) {
 	FILE* src = fopen("nscript.dat", "r+b");
 	if(src == NULL) {
-		printf("unable to find/open nscript.dat, searching for nscript.txt...\n");
+		puts("unable to find/open nscript.dat, searching for nscript.txt...");
 		src = fopen("nscript.txt", "r+b");
 		if(src == NULL) {
-			printf("not found. execution terminated\n");
-			return;
+			puts("no suitable files found. execution terminated");
+			return 1;
 		}
 		FILE* out = fopen("nscript.dat", "w+b");
 		if(out == NULL) {
-			printf("unable to create output file\n");
-			return;
+			puts("unable to create output file");
+			return 1;
 		}
 		write(src, out);
-		printf("nscript.txt encrypted in nscript.dat\n");
-		#ifdef _WIN32
-		system("pause");
-		#endif
-		#ifdef __unix__
-		system("bash -c 'read -r -p \"Press any key to continue...\" && command'");
-		#endif
-		return;
+		puts("nscript.txt encrypted in nscript.dat");
+		puts("Press any key to continue...");
+		return 0;
 	}
 	FILE* out = fopen("nscript.txt", "w+b");
 	write(src, out);
-	printf("nscript.dat decrypted in nscript.txt\n");
-	#ifdef _WIN32
-	system("pause");
-	#endif
-	#ifdef __unix__
-	system("bash -c 'read -r -p \"Press any key to continue...\" && command'");
-	#endif
+	printf("nscript.dat decrypted in nscript.txt");
+	puts("Press any key to continue");
+	getchar();
+	return 0;
 }
